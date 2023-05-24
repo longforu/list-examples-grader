e6a71916e14bd0b3d786edc5ba9dcbc85a8339da
Long Tran <longtrandeveloper@gmail.com>
2023-05-24 15:23:32 -0700

    push

--- TestListExamples.java
+++ TestListExamples.java
@@ -18,4 +18,23 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+  @Test
+  public void testFilter1() {
+    List<String> input1 = Arrays.asList("a", "b", "c");
+    List<String> result = ListExamples.filter(input1, new StringChecker() {
+      public boolean checkString(String s) {
+        return true;
+      }
+    });
+    assertEquals(input1, result);
+  }
+
+  // test that the function is not in an infinite loop
+  @Test (timeout=1000)
+  public void testMerge1() {
+    List<String> input1 = Arrays.asList("a", "b", "c");
+    List<String> input2 = Arrays.asList("d");
+    List<String> result = ListExamples.merge(input1, input2);
+    assertEquals(4, result.size());
+  }
 }
--- grade.sh
+++ grade.sh
@@ -14,3 +14,34 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+# check that there is a ListExamples file
+if [ -f ./student-submission/ListExamples.java ]; then
+  cp  -r ./student-submission/ ./grading-area
+  cp -r ./lib ./grading-area
+  cp ./TestListExamples.java ./grading-area/TestListExamples.java
+  echo "Done copying"
+
+  # after copying the file, test the thing
+  set +e
+  
+  javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar ./grading-area/*.java 2> ./report.txt
+
+  if [ $? -ne 0 ]; then
+    cat ./report.txt
+    echo "Your files failed to compile"
+    echo "Your files failed to compile." >> ./report.txt
+    exit
+  fi
+
+  cd ./grading-area
+  java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > ../report.txt
+  cd ../
+
+else
+  echo "File does not exist."
+  echo "File does not exist." > ./report.txt
+  exit
+fi
+
+cat ./report.txt
